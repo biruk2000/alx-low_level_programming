459313962f38bad9aa193bea84a4d5689b2d7970
Biruk Molla <birukmolla30@gmail.com>
2021-02-03 18:08:35 +0000

    C program that prints some text

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,5 +1,13 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point
+ *
+ * printf: prints a string
+ *
+ * Return: Always 0 (Success)
+ */
+
 int main(void)
 {
 printf("with proper grammar, but the outcome is a piece of art,\n");
