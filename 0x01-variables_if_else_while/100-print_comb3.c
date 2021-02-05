e296f2bd91e03b1c396ef0a8faf18921ec811d96
Biruk Molla <birukmolla30@gmail.com>
2021-02-05 16:27:41 +0000

    prints all possible different combinations of two digits

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -9,17 +9,19 @@ int main(void)
 {
 int i = '0';
 int j = '1';
-int k= '1';
+int k = '1';
 while (i <= '8')
 {
 while (j <= '9')
 {
+if(i != j){
 putchar(i);
 putchar(j);
 if(i != '8')
 {
 putchar(',');
 putchar(' ');
+}
 }
 j++;
 }
