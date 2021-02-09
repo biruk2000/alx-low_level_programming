3cf13915ddede63568073ec4182288616b0d70c7
Biruk Molla <birukmolla30@gmail.com>
2021-02-09 17:52:27 +0000

    a program that finds and prints the first 98 Fibonacci numbers

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - prints the first 50 Fibonacci numbers, starting with 1 and 2
+ *
+ * Return: Always 0 (Success).
+ */
+
+int main(void)
+{
+int i;
+long int num1 = 0, num2 = 1, sum;
+for (i = 0; i < 97; i++)
+{
+sum = num1 + num2;
+num1 = num2;
+num2 = sum;
+printf("%ld", sum);
+if (i != 96)
+printf(", ");
+}
+printf("\n");
+return (0);
+}
