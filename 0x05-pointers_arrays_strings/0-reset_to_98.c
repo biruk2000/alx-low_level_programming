4c5e9902e5266eb03e3593b7b68b8cc9f4a80dee
biruk molla <birukmolla30@gmail.com>
2021-02-16 17:14:16 +0000

    function that takes a pointer

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,11 +1,12 @@
 #include "holberton.h"
 
 /**
- * 0-reset_to_98.c - pointer to an int as parameter and updates the value it points to to 98.
+ * reset_to_98 - updates the value pointed to 98
  *
- * Return: void.
+ * @n: value of adress n
+ *
+ * Return: void
  */
-
 void reset_to_98(int *n)
 {
 *n = 98;
