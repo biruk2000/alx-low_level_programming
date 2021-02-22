ac5c21d01dfd093d6c4a1258720bceb257ec7d0f
biruk molla <birukmolla30@gmail.com>
2021-02-22 18:54:29 +0000

    function that locates a substring

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+
+/**
+ * _strstr - locate a substring
+ *
+ * @haystack: string to check
+ * @needle: substring
+ *
+ * Return: substring or NULL if not located
+ */
+char *_strstr(char *haystack, char *needle)
+{
+while (*haystack)
+{
+char *Begin = haystack;
+char *pattern = needle;
+while (*haystack && *pattern && *haystack == *pattern)
+haystack++,
+pattern++;
+if (!*pattern)
+return (Begin);
+haystack = Begin + 1;
+}
+return (0);
+}
