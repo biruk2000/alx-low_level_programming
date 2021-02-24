da7664f4dde910d69338fd745d30352c894fb881
biruk molla <birukmolla30@gmail.com>
2021-02-24 14:22:30 +0000

    recurtion funcitons

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+
+/**
+ * _strlen_recursion - counts the length of a string
+ * @s: input string
+ * Return: int
+ */
+
+int _strlen_recursion(char *s)
+{
+if (*s)
+{
+return (1 + _strlen_recursion(s + 1));
+}
+return (0);
+}
---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+
+/**
+ * _pow_recursion - returns the factorial of a given number
+ * @x: input 1
+ * @y: input 2
+ * Return: int
+ */
+
+int _pow_recursion(int x, int y)
+{
+if (y < 0)
+{
+return (-1);
+}
+else if (y == 0)
+{
+return (1);
+}
+return (x * _pow_recursion(x, y - 1));
+}
